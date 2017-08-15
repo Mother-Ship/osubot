d6a933267b17739dc76734592e7e1fb61794b513
瞿瀚盛 <quhansheng@gmail.com>
2017-08-16 07:56:19 +0800

    Initial commit

---
+++ src/main/java/top/mothership/osubot/App.java
@@ -0,0 +1,13 @@
+package top.mothership.osubot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
